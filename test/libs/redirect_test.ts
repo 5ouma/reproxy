42c072c3ec1ce9640bfc951fe0a644a0adac2fda
5ouma <101255979+5ouma@users.noreply.github.com>
2024-06-05 22:42:56 +0900

    test(util): Pull out test variables or a function
    To keep code clean, and make it easy to change the ingredients.

--- test/libs/redirect_test.ts
+++ test/libs/redirect_test.ts
@@ -3,42 +3,28 @@ import { assertEquals } from "@std/assert";
 import { STATUS_CODE } from "@std/http/status";
 import { UserAgent } from "@std/http/user-agent";
 import { redirect } from "../../src/libs/redirect.ts";
-import { Repository } from "../../src/types/repository.ts";
+import { unknownRepo, testRef, exportRepo } from "../utils.ts";
 
 Deno.test("Redirect Detection", <R extends string>() => {
   const ctx: RouterContext<R> = testing.createMockContext();
-  const repository: Repository = {
-    owner: "repository-owner",
-    name: "repository-name",
-    path: "repository-path",
-  };
-  Deno.env.set("REPOSITORY_OWNER", repository.owner);
-  Deno.env.set("REPOSITORY_NAME", repository.name);
-  Deno.env.set("REPOSITORY_PATH", repository.path);
+  exportRepo(unknownRepo);
 
   redirect(ctx, new UserAgent("Chrome/1.2.3"));
   assertEquals(ctx.response.status, STATUS_CODE.PermanentRedirect);
   assertEquals(
     ctx.response.headers.get("location"),
-    "https://github.com/repository-owner/repository-name/blob/HEAD/repository-path"
+    `https://github.com/${unknownRepo.owner}/${unknownRepo.name}/blob/HEAD/${unknownRepo.path}`
   );
 });
 
 Deno.test("Redirect Detection (With ref)", <R extends string>() => {
   const ctx: RouterContext<R> = testing.createMockContext();
-  const repository: Repository = {
-    owner: "repository-owner",
-    name: "repository-name",
-    path: "repository-path",
-  };
-  Deno.env.set("REPOSITORY_OWNER", repository.owner);
-  Deno.env.set("REPOSITORY_NAME", repository.name);
-  Deno.env.set("REPOSITORY_PATH", repository.path);
+  exportRepo(unknownRepo);
 
-  redirect(ctx, new UserAgent("Chrome/1.2.3"), "v1.0.0");
+  redirect(ctx, new UserAgent("Chrome/1.2.3"), testRef);
   assertEquals(ctx.response.status, STATUS_CODE.PermanentRedirect);
   assertEquals(
     ctx.response.headers.get("location"),
-    "https://github.com/repository-owner/repository-name/blob/v1.0.0/repository-path"
+    `https://github.com/${unknownRepo.owner}/${unknownRepo.name}/blob/${testRef}/${unknownRepo.path}`
   );
 });
---
+++ test/utils.ts
@@ -0,0 +1,21 @@
+import type { Repository } from "../src/types/mod.ts";
+
+export const testRepo: Repository = {
+  owner: "denoland",
+  name: "deno",
+  path: "README.md",
+};
+
+export const unknownRepo: Repository = {
+  owner: "unknown-owner",
+  name: "unknown-repo",
+  path: "unknown-path",
+};
+
+export const testRef = "v1.0.0";
+
+export function exportRepo(repository: Repository) {
+  Deno.env.set("REPOSITORY_OWNER", repository.owner);
+  Deno.env.set("REPOSITORY_NAME", repository.name);
+  Deno.env.set("REPOSITORY_PATH", repository.path);
+}
