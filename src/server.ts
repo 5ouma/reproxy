86fab9542f83043f69a8122fd958d58af291361c
5ouma <101255979+5ouma@users.noreply.github.com>
2024-06-05 22:49:02 +0900

    chore(dev): Useful dev command and messages
    Run watcher process, and show the message for hosting address.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import { Application } from "@oak/oak";
+import type { ApplicationListenEvent } from "@oak/oak/application";
+import { yellow } from "@std/fmt/colors";
 import { router } from "./router.ts";
 
 const app = new Application();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+app.addEventListener(
+  "listen",
+  ({ secure, hostname, port }: ApplicationListenEvent) => {
+    console.log(
+      `🔔 listening: ${yellow(
+        `${secure ? "https" : "http"}://${hostname ?? "localhost"}:${port}`
+      )}`
+    );
+  }
+);
 
 await app.listen({ port: 8080 });
