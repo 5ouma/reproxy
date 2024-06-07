dc69f2d0b3762a2ac4b8689175ef69e8bbdba59d
5ouma <101255979+5ouma@users.noreply.github.com>
2024-06-07 19:06:09 +0900

    style(test): Write more clean test code
    Tests are meaningful and readable.

--- test/libs/content_test.ts
+++ test/libs/content_test.ts
@@ -7,24 +7,24 @@ import { testRepo, unknownRepo, testRef, exportRepo } from "../utils.ts";
 Deno.test("Get Content", async <R extends string>() => {
   const ctx: RouterContext<R> = testing.createMockContext();
   exportRepo(testRepo);
-
   await getContent(ctx);
+
   assertEquals(ctx.response.status, STATUS_CODE.OK);
 });
 
 Deno.test("Get Content (With ref)", async <R extends string>() => {
   const ctx: RouterContext<R> = testing.createMockContext();
   exportRepo(testRepo);
-
   await getContent(ctx, testRef);
+
   assertEquals(ctx.response.status, STATUS_CODE.OK);
 });
 
 Deno.test("Get Content (Not found)", async <R extends string>() => {
   const ctx: RouterContext<R> = testing.createMockContext();
   exportRepo(unknownRepo);
-
   await getContent(ctx);
+
   assertEquals(ctx.response.status, STATUS_CODE.NotFound);
   assertStringIncludes(
     ctx.response.body!.toString(),
