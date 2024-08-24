34ca55e71dfba5d88f7f6815e52689a20d2d3210
5ouma <101255979+5ouma@users.noreply.github.com>
2024-08-24 15:29:09 +0900

    refactor(hono): Use the optional path parameter
    Combine the root and ref endpoints.

--- src/server.ts
+++ src/server.ts
@@ -5,32 +5,21 @@ import { UserAgent } from "@std/http/user-agent";
 import { getContent, redirect } from "./libs/mod.ts";
 
 export const app = new Hono();
+app
+  .get("/:ref?", async (ctx: Context) => {
+    const ref: string = ctx.req.param("ref");
 
-app.get("/", async (ctx: Context) => {
-  const url: URL | null = redirect(
-    new UserAgent(ctx.req.header("User-Agent") ?? ""),
-  );
-  if (url) return ctx.redirect(url.toString(), STATUS_CODE.PermanentRedirect);
+    const url: URL | null = redirect(
+      new UserAgent(ctx.req.header("User-Agent") ?? ""),
+      ref,
+    );
+    if (url) return ctx.redirect(url.toString(), STATUS_CODE.PermanentRedirect);
 
-  const [data, status] = await getContent();
-  return ctx.text(data, status);
-});
-
-app.get("/:ref", async (ctx: Context) => {
-  const ref: string = ctx.req.param("ref");
-
-  const url: URL | null = redirect(
-    new UserAgent(ctx.req.header("User-Agent") ?? ""),
-    ref,
-  );
-  if (url) return ctx.redirect(url.toString(), STATUS_CODE.PermanentRedirect);
-
-  const [data, status] = await getContent(ref);
-  return ctx.text(data, status);
-});
-
-app.get("*", (ctx: Context) => {
-  return ctx.redirect("/", STATUS_CODE.SeeOther);
-});
+    const [data, status] = await getContent(ref);
+    return ctx.text(data, status);
+  })
+  .get("*", (ctx: Context) => {
+    return ctx.redirect("/", STATUS_CODE.SeeOther);
+  });
 
 Deno.serve(app.fetch);
