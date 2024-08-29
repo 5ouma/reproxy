aa2def04be47c2e64fbff1a5e8689dccb8ebd3e1
Souma <101255979+5ouma@users.noreply.github.com>
2024-08-30 08:49:34 +0900

    feat(jsr): Publish to JSR (#47)
    Stop using the dev branch to publish this project to JSR.

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "@std/assert";
 import { STATUS_CODE } from "@std/http/status";
 
-import { app } from "./server.ts";
+import { app } from "./app.ts";
 import {
   exportRepo,
   testRef,
