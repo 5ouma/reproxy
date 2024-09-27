539fae169c07cc0c511c0b692f9b76f026e1cac5
Souma <101255979+5ouma@users.noreply.github.com>
2024-09-27 15:54:02 +0900

    feat(hono): Make workable on any runtime (#64)
    Use the env adapter to avoid Deno-dedicated API.

--- src/libs/content.test.ts
+++ src/libs/content.test.ts
@@ -2,28 +2,25 @@ import { assertEquals, assertExists, assertStringIncludes } from "@std/assert";
 import { STATUS_CODE } from "@std/http/status";
 
 import { getContent } from "./content.ts";
-import { exportRepo, testRef, testRepo, unknownRepo } from "./test_utils.ts";
+import { testRef, testRepo, unknownRepo } from "./test_utils.ts";
 
 Deno.test("Get Content", async (t: Deno.TestContext) => {
   await t.step("normal", async () => {
-    exportRepo(testRepo);
-    const [data, status] = await getContent();
+    const [data, status] = await getContent(testRepo);
 
     assertExists(data);
     assertEquals(status, STATUS_CODE.OK);
   });
 
   await t.step("with ref", async () => {
-    exportRepo(testRepo);
-    const [data, status] = await getContent(testRef);
+    const [data, status] = await getContent(testRepo, testRef);
 
     assertExists(data);
     assertEquals(status, STATUS_CODE.OK);
   });
 
   await t.step("not found", async () => {
-    exportRepo(unknownRepo);
-    const [data, status] = await getContent();
+    const [data, status] = await getContent(unknownRepo);
 
     assertStringIncludes(data, `⚠️ ${STATUS_CODE.NotFound}:`);
     assertEquals(status, STATUS_CODE.NotFound);
--- src/libs/redirect.ts
+++ src/libs/redirect.ts
@@ -1,7 +1,7 @@
 import type { UserAgent } from "@std/http/user-agent";
 export type { UserAgent };
 
-import { getRepository } from "./env.ts";
+import type { Repository } from "./types.ts";
 import { getGitHubUrl } from "./utils.ts";
 
 /**
@@ -14,32 +14,53 @@ import { getGitHubUrl } from "./utils.ts";
  * @example Use the default branch
  * ```ts
  * const userAgent = new UserAgent("Chrome/1.2.3");
- * const url: URL | null = checkRedirect(userAgent);
+ * const repository: Repository = {
+ *   owner: "denoland",
+ *   name: "deno",
+ *   path: "README.md",
+ * };
+ * const url: URL | null = checkRedirect(userAgent, repository);
  * ```
  * @example Use a specific branch
  * ```ts
  * const userAgent = new UserAgent("Chrome/1.2.3");
+ * const repository: Repository = {
+ *   owner: "denoland",
+ *   name: "deno",
+ *   path: "README.md",
+ * };
  * const branch = "main";
- * const url: URL | null = checkRedirect(userAgent, branch);
+ * const url: URL | null = checkRedirect(userAgent, repository, branch);
  * ```
  * @example Use a specific tag
  * ```ts
  * const userAgent = new UserAgent("Chrome/1.2.3");
+ * const repository: Repository = {
+ *   owner: "denoland",
+ *   name: "deno",
+ *   path: "README.md",
+ * };
  * const tag = "v1.0.0";
- * const url: URL | null = checkRedirect(userAgent, tag);
+ * const url: URL | null = checkRedirect(userAgent, repository, tag);
  * ```
  * @example Use a specific commit
  * ```ts
  * const userAgent = new UserAgent("Chrome/1.2.3");
+ * const repository: Repository = {
+ *   owner: "denoland",
+ *   name: "deno",
+ *   path: "README.md",
+ * };
  * const commit = "a1b2c3d4e5f6";
- * const url: URL | null = checkRedirect(userAgent, commit);
+ * const url: URL | null = checkRedirect(userAgent, repository, commit);
  * ```
  */
 export function checkRedirect(
   userAgent: UserAgent,
+  repository: Repository,
   ref: string = "master",
 ): URL | null {
-  const url = getGitHubUrl(getRepository(), ref);
+  const url = getGitHubUrl(repository, ref);
 
   return userAgent?.browser.name ? url : null;
 }
