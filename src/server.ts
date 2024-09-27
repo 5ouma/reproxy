3cf54d7682a8b4c6cee962211af2f1d36ec90027
Souma <101255979+5ouma@users.noreply.github.com>
2024-09-27 15:36:54 +0900

    feat(hono): Use env adapter
    Make Reproxy workable on any runtime.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import { type Context, Hono } from "@hono/hono";
 export type { Hono };
+import { env } from "@hono/hono/adapter";
 import { logger } from "@hono/hono/logger";
 import { STATUS_CODE } from "@std/http/status";
 import { UserAgent } from "@std/http/user-agent";
 
 import { checkRedirect, getContent, type Repository } from "./libs/mod.ts";
-import { getRepository, githubToken } from "./libs/env.ts";
 
 /**
  * The Hono application for this project.
@@ -25,7 +25,20 @@ export const app: Hono = new Hono();
 app.use(logger());
 app
   .get("/:ref?", async (ctx: Context) => {
-    const repository: Repository = getRepository();
+    const { REPOSITORY_OWNER, REPOSITORY_NAME, REPOSITORY_PATH, GITHUB_TOKEN } =
+      env<
+        {
+          REPOSITORY_OWNER: string;
+          REPOSITORY_NAME: string;
+          REPOSITORY_PATH: string;
+          GITHUB_TOKEN: string;
+        }
+      >(ctx);
+    const repository: Repository = {
+      owner: REPOSITORY_OWNER,
+      name: REPOSITORY_NAME,
+      path: REPOSITORY_PATH,
+    };
     const ref: string = ctx.req.param("ref");
 
     const url: URL | null = checkRedirect(
@@ -36,7 +49,7 @@ app
 
     return url
       ? ctx.redirect(url.toString(), STATUS_CODE.PermanentRedirect)
-      : ctx.text(...await getContent(repository, ref, githubToken));
+      : ctx.text(...await getContent(repository, ref, GITHUB_TOKEN));
   })
   .get("*", (ctx: Context) => {
     return ctx.redirect("/", STATUS_CODE.SeeOther);
