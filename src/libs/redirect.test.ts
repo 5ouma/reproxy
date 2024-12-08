e2576421700d78011dedc9ef4bed3d1c65c52642
Souma <101255979+5ouma@users.noreply.github.com>
2024-12-08 18:05:20 +0900

    test(redirect): Check not redirect cases
    Check null is returned when not accessed by a browser.

--- src/libs/redirect.test.ts
+++ src/libs/redirect.test.ts
@@ -1,23 +1,44 @@
 import { assertEquals } from "@std/assert";
+import { UserAgent } from "@std/http/user-agent";
 
 import { checkRedirect } from "./redirect.ts";
 import { testRef, testRepo, testUserAgent } from "./test_utils.ts";
 import { getGitHubUrl } from "./utils.ts";
 
-Deno.test("Redirect Detection", async (t: Deno.TestContext) => {
-  await t.step("normal", () => {
-    const url: URL | null = checkRedirect(testUserAgent, testRepo.normal);
+Deno.test("Redirect Detection", () => {
+  Deno.test("Direct", async (t: Deno.TestContext) => {
+    await t.step("normal", () => {
+      const url: URL | null = checkRedirect(new UserAgent(""), testRepo.normal);
 
-    assertEquals(url, getGitHubUrl(testRepo.normal));
+      assertEquals(url, null);
+    });
+
+    await t.step("with ref", () => {
+      const url: URL | null = checkRedirect(
+        new UserAgent(""),
+        testRepo.normal,
+        testRef.normal,
+      );
+
+      assertEquals(url, null);
+    });
   });
 
-  await t.step("with ref", () => {
-    const url: URL | null = checkRedirect(
-      testUserAgent,
-      testRepo.normal,
-      testRef.normal,
-    );
+  Deno.test("Redirect", async (t: Deno.TestContext) => {
+    await t.step("normal", () => {
+      const url: URL | null = checkRedirect(testUserAgent, testRepo.normal);
 
-    assertEquals(url, getGitHubUrl(testRepo.normal, testRef.normal));
+      assertEquals(url, getGitHubUrl(testRepo.normal));
+    });
+
+    await t.step("with ref", () => {
+      const url: URL | null = checkRedirect(
+        testUserAgent,
+        testRepo.normal,
+        testRef.normal,
+      );
+
+      assertEquals(url, getGitHubUrl(testRepo.normal, testRef.normal));
+    });
   });
 });
